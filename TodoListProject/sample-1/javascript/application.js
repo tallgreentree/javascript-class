3ff25bdef619f08734d95be1237a45c368f938c5
Will Barrett <william.barrett@tallgreentree.com>
2012-03-20 08:59:02 -0500

    Example todo list application added

---
+++ TodoListProject/sample-1/javascript/application.js
@@ -0,0 +1,29 @@
+var TodoList = {
+        init: function() {
+                      this.form = document.getElementById('todo-form');
+                      this.form.onsubmit = TodoList.addTodo;
+                      this.list = document.getElementById('todo-list');
+              },
+        addTodo: function() {
+                      var input = TodoList.form.getElementsByTagName('input')[0];
+                      var content = document.createTextNode(input.value);
+                      var newitem = document.createElement('li');
+                      var checkbox = document.createElement('input');
+                      checkbox.setAttribute('type', 'checkbox');
+                      checkbox.onclick = TodoList.completeTodo;
+                      
+                      newitem.appendChild(checkbox);
+                      newitem.appendChild(content);
+                      TodoList.list.appendChild(newitem);
+                      return false;
+               },
+        completeTodo: function() {
+                      var li = this.parentElement;
+                      li.parentElement.removeChild(li);
+        }
+};
+
+
+window.onload = function() {
+        TodoList.init();
+}
